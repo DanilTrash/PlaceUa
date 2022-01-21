71b08b1e78788ec5764c19c23aacb4d9f07f8bf1
daniltrash <lindamscscout111@gmail.com>
2022-01-21 16:24:16 +0200

    bug fixed

--- __main__.py
+++ __main__.py
@@ -14,26 +14,28 @@ logger = log(__name__)
 
 
 class Placeua:
-    def __init__(self, proxy, headless=False):
+    def __init__(self, proxy, login, password, headless=False):
         options = webdriver.ChromeOptions()
+        self.login = login
+        self.password = password
         if headless:
             options.add_argument('--headless')
         options.add_argument('--proxy-server=%s' % proxy)
         self.driver = webdriver.Chrome(options=options)
-        self.driver.minimize_window()
+        # self.driver.minimize_window()
         logger.info(options.arguments)
 
-    def auth(self, login, password):
+    def auth(self):
         self.driver.get('https://place.ua/user/login')
-        logger.info([login, password])
+        logger.info([self.login, self.password])
         WebDriverWait(self.driver, 10).until(
             lambda d: self.driver.execute_script('return document.readyState;') == 'complete')
 
         email_xpath = '//*[@id="j-u-login-email"]'
         WebDriverWait(self.driver, 10).until(
-            EC.visibility_of_element_located((By.XPATH, email_xpath))).send_keys(login)
+            EC.visibility_of_element_located((By.XPATH, email_xpath))).send_keys(self.login)
         password_xpath = '//*[@id="j-u-login-pass"]'
-        self.driver.find_element_by_xpath(password_xpath).send_keys(password)
+        self.driver.find_element_by_xpath(password_xpath).send_keys(self.password)
 
         submit_xpath = '//*[@type="submit"]'
         self.driver.find_element_by_xpath(submit_xpath).click()
@@ -45,71 +47,62 @@ class Placeua:
         except TimeoutException as error:
             logger.warning(error)
             return False
-        logger.info('Auth: True')
         return True
 
     def dobavit_obyavlenie(self, title, description, phone):
         logger.info(title)
         self.driver.get('https://place.ua/item/add')
-        WebDriverWait(self.driver, 10).until(
-            lambda d: self.driver.execute_script('return document.readyState;') == 'complete')
+        category_xpath = '//span[text()="Начало карьеры, студенты"]'
+        price_xpath = '//*[@name="price"]'
+        phone_xpath = '//*[@name="phones[1]"]'
         title_xpath = '//*[@id="j-i-title"]'
         self.driver.find_element_by_xpath(title_xpath).send_keys(title)
         j_i_form_xpath = '//*[@id="j-i-form"]/div[2]/div/div/div[2]/a'
         self.driver.find_element_by_xpath(j_i_form_xpath).click()
         rabota_xpath = '//*[@id="j-i-form"]/div[2]/div/div/div[3]/div/div[1]/div/a[9]/img'
         self.driver.find_element_by_xpath(rabota_xpath).click()
-        category_xpath = '//span[text()="Начало карьеры, студенты"]'
         WebDriverWait(self.driver, 10).until(EC.presence_of_element_located((By.XPATH, category_xpath))).click()
-        price_xpath = '//*[@name="price"]'
         WebDriverWait(self.driver, 10).until(
-            EC.presence_of_element_located((By.XPATH, price_xpath))).send_keys(f'1{str(choice(range(6, 9)))}000')
+            EC.presence_of_element_located((By.XPATH, price_xpath))).send_keys(f'2{str(choice(range(0, 5)))}000')
         val_xpath = '//select[@name="d[6][572]"]/option[2]'
         self.driver.find_element_by_xpath(val_xpath).click()
         val_xpath = '//select[@name="d[6][571]"]/option[2]'
         self.driver.find_element_by_xpath(val_xpath).click()
         description_xpath = '//*[@id="j-i-descr"]'
-        self.driver.find_element_by_xpath(description_xpath).send_keys(description)
+        for letter in description:
+            self.driver.find_element_by_xpath(description_xpath).send_keys(letter)
         hr_xpath = '//*[@id="j-i-name"]'
         element = self.driver.find_element_by_xpath(hr_xpath)
         element.clear()
         element.send_keys(choice(['Юлия', 'Алёна']))
-        phone_xpath = '//*[@name="phones[1]"]'
         self.driver.find_element_by_xpath(phone_xpath).send_keys(phone)
         submit_xpath = '//*[@value="Опубликовать объявление"]'
         element = self.driver.find_element_by_xpath(submit_xpath)
         element.click()
         return True
 
-    def end(self):
-        logger.info('ending session')
-        self.driver.quit()
-
 
 def main():
-    df = pd.read_csv('https://docs.google.com/spreadsheets/d/1zaxjdu9ESYy2MCNuDow0_5PnZpwEsyrdTQ_kk0PMZbw/'
-                     'export?format=csv&id=1zaxjdu9ESYy2MCNuDow0_5PnZpwEsyrdTQ_kk0PMZbw&gid=892686484',
-                     dtype={'phone': str, 'proxy': str})
-    titles = df['title'].dropna().tolist()
-    descriptions = df['description'].dropna().tolist()
-    phones = df['phone'].dropna().tolist()
+    from itertools import cycle
+    df = pd.read_csv('https://docs.google.com/spreadsheets/d/1zaxjdu9ESYy2MCNuDow0_5PnZpwEsyr'
+                     'dTQ_kk0PMZbw/export?format=csv&id=1zaxjdu9ESYy2MCNuDow0_5PnZ'
+                     'pwEsyrdTQ_kk0PMZbw&gid=892686484', dtype={'phone': str, 'proxy': str})
+    titles = cycle(df['title'].dropna().tolist())
+    descriptions = cycle(df['description'].dropna().tolist())
+    phones = cycle(df['phone'].dropna().tolist())
     logins = df['login'].dropna().tolist()
     passwords = df['password'].dropna().tolist()
-    proxy = df['proxy'].dropna().tolist()[0]
+    proxy = df['proxy'].dropna().tolist()
     headless = False
-    for i, _ in enumerate(titles):
-        place = Placeua(proxy, headless=headless)
-        place.auth(logins[i], passwords[i])
-        place.dobavit_obyavlenie(titles[i], descriptions[i], phones[i])
-        place.end()
-        logger.info('sleeping')
-        # sleep(35)
+    place = Placeua(proxy[0], logins[0], passwords[0], headless=headless)
+    place.auth()
+    while True:
+        place.dobavit_obyavlenie(next(titles), next(descriptions), next(phones))
 
 
 if __name__ == '__main__':
-    while True:
-        try:
-            logger.info(__name__)
-            main()
-        except Exception as error:
-            logger.exception(error)
+    try:
+        logger.info(__name__)
+        main()
+    except Exception as error:
+        logger.exception(error)
